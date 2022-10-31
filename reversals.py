e314e63be9cada0cb988ffc294bf5f25b3736c9c
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-10-31 18:07:27 +0300

    added some changes

---
+++ reversals.py
@@ -0,0 +1,21 @@
+############################################################################
+## 									   ##
+##	Title : 							   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# A simple reverse cipher.
+
+word = input(" I do reversals, enter something 😐️ : ")
+
+num = len(word)
+
+while num > 0:
+	letter = word[num-1]
+	num -= 1
+	print(letter, end="")
+	
+	
